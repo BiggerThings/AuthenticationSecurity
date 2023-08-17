5dbec31822789890472ccd2ae949a76a2b85bdd6
BiggerThings <nnsbiggerthings@gmail.com>
2023-08-17 15:59:38 +0200

    Hashing passwords

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const SHA256 = require('crypto-js/sha256');
 
 const app = express();
 
-console.log(process.env.SECRET);
+console.log(SHA256('1234asdf').toString());
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret : process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -40,7 +39,7 @@ app.get('/register', function (req, res) {
 app.post('/register', function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: SHA256(req.body.password).toString()
     });
 
     newUser.save()
@@ -54,7 +53,7 @@ app.post('/register', function (req, res) {
 
 app.post('/login', function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = SHA256(req.body.password).toString();
 
     User.findOne({ email: username})
     .then((foundUser) => {
